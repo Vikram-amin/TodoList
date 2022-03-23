23509ea38850a41dd367c3d34c09651e23a628d4
Vikram <vikramamin22@gmail.com>
2022-03-23 17:54:09 +0530

    React

--- my-app/src/Components/Todo.jsx
+++ my-app/src/Components/Todo.jsx
@@ -1,63 +1,69 @@
-import React,{useState,useEffect} from 'react'
-import TodoInput from './TodoInput'
-import { v4 as uuid } from "uuid";
+
+import React from "react";
+import TodoInput from "./TodoInput"
 
 function Todo() {
+  const [todos, setTodos] = React.useState([]);
 
-  const [data,setData]  = useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [isError, setIsError] = React.useState(false);
+  const [page, setPage] = React.useState(1);
 
-    const [isLoading, setIsLoading] = useState(true);
-    const [isError, setIsError] = useState(false);
-    const [page, setPage] = useState(1);
+  React.useEffect(() => {
+    getTodos(); // invoke when components mounted for the first time
+  }, [page]); // when page changes call this
 
-    useEffect(() => {
-      getTodos()
-    },[page])
+  const getTodos = () => {
+    fetch(`http://localhost:3001/todoData?_page=${page}&_limit=3`)
+      .then((res) => res.json())
+      .then((res) => {
+        setTodos(res);
+        setIsError(false);
+      })
+      .catch((err) => setIsError(true))
+      .finally(() => setIsLoading(false));
+  };
 
-    const getTodos = () => {
-      fetch(`http://localhost:3001/todos?_page=${page}&_limit=3`)
-        .then((res) => res.json())
-        .then((res) => {
-           setData(res);
-           setIsError(false);
-        })
-        .catch((err) => setIsError(true))
-        .finally(() => setIsLoading(false));
+  const handleAdd = (query) => {
+    const PayLoad = {
+      title: query,
+      status: false,
     };
 
- const handleAdd = (title) => {
-   const payload = {
-     title,
-     status: false,
-   };
+    const payLoadJson = JSON.stringify(PayLoad);
 
-   const payLoadJSON = JSON.stringify(payload);
+    setIsLoading(true);
+    fetch(`http://localhost:3001/todoData`, {
+      method: "POST",
+      body: payLoadJson,
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+      .then((res) => {
+        getTodos();
+      })
+      .catch((err) => setIsError(true))
+      .finally(() => setIsLoading(false));
+  };
 
-   setIsLoading(true);
-   fetch(`http://localhost:3001/todoData`, {
-     method: "POST",
-     body: "payLoadJSON",
-     headers: {
-       "content-type": "application/json",
-     },
-   })
-     .then(() => {
-       getTodos();
-     })
-     .catch((err) => setIsError(true))
-     .finally(() => setIsLoading(false));
-  }
- 
-
-  return (
+  return isLoading ? (
+    <div> ....Loading </div>
+  ) : isError ? (
+    <div>....Error ... Something Went Wrong</div>
+  ) : (
     <div>
       <TodoInput handleAddprops={handleAdd} />
 
-      {data.map((item) => {
+      {todos.map((item) => {
         return <div>{item.title}</div>;
       })}
+
+      <button onClick={() => setPage(page - 1)}>Prev</button>
+      <button onClick={() => setPage(page + 1)}>Next</button>
     </div>
   );
 }
 
-export default Todo
+export default Todo;
+
--- my-app/src/Components/TodoItems.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function TodoItems() {
-  return (
-    <div>TodoItems</div>
-  )
-}
-
-export default TodoItems
